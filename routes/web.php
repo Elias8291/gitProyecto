ab5891541fcc42d70bed54010c3f0577ed7c5176
Angel Rafael Cruz Chavez <162520560+EskelerSiu@users.noreply.github.com>
2024-05-10 10:44:09 -0600

    IntentoCorreo

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,12 @@ use App\Http\Controllers\LogController;
 
 // Ruta para la página de bienvenida, accesible para todos los usuarios
 Route::get('/', [WelcomeController::class, 'showWelcomePage'])->name('welcome');
-
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/logs', 'LogController@fetchLogs')->name('logs.fetch');
 
 Auth::routes();
 
-// Rutas para la recuperación de contraseña
-Route::get('/password/reset', [Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-Route::post('/password/email', [Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('/password/reset/{token}', [Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('/password/reset', [Auth\ResetPasswordController::class, 'reset']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [WelcomeController::class, 'showWelcomePage'])->name('welcome');
 
 // Grupo de rutas protegidas por el middleware 'auth'
 Route::group(['middleware' => ['auth']], function() {
@@ -38,5 +33,4 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('materias', MateriasController::class);
     Route::resource('logs', LogController::class);
     Route::get('/grupos/{clave}/generarPDF', [GrupoController::class, 'generarPDF'])->name('grupos.generarPDF');
-    Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLink'])->name('password.email');
 });
