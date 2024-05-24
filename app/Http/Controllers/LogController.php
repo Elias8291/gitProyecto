d1ff4a520023998bb58d3ab8ce60dc46274a8352
CarlosRendon01 <carlosrendonx01@gmail.com>
2024-05-24 17:05:00 -0600

    LogCorrectos

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -17,17 +17,15 @@ class LogController extends Controller
     public function index()
     {
         $logs = DB::table('logs')
-            ->select('id', 'created_at', 'action', 'table', 'record_id', 'executedSQL', 'reverseSQL', 'user_name')
+            ->select('id',  DB::raw('DATE_FORMAT(created_at, "%d de %M de %Y - %H:%i:%s") as formatted_date'), 'action', 'table', 'record_id', 'executedSQL', 'reverseSQL', 'user_name')
             ->orderBy('created_at', 'desc')
             ->paginate(30); // Ajusta el número de registros por página como prefieras
-    
-        // Formatear las fechas en letras y en español
-        foreach ($logs as $log) {
-            $log->formatted_date = Carbon::parse($log->created_at)->locale('es')->isoFormat('D [de] MMMM [de] YYYY');
-        }
-    
+            date_default_timezone_set('America/Mexico_City');
         return view('logs.index', compact('logs'));
+        
     }
     
+
+
     
 }
